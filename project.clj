7eae3ff72a44ff6c9faff55e4417d2b2760424cc
Rui Guo <rguo@leancloud.rocks>
2015-08-21 17:56:58 +0800

    (feature) add internal counter

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.ylgrgyq/carmine-mock-tool "0.2.0-SNAPSHOT"
+(defproject org.clojars.ylgrgyq/carmine-mock-tool "0.2.1-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
-                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
+                      :url  "https://github.com/ylgrgyq/carmine-mock-tool"}
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [com.taoensso/carmine "2.9.0"]]
             :target-path "target/%s"
--- src/carmine_mock_tool/core.clj
+++ src/carmine_mock_tool/core.clj
@@ -5,6 +5,8 @@
            (taoensso.carmine.connections NonPooledConnectionPool Connection)
            (taoensso.carmine.connections NonPooledConnectionPool)))
 
+(def ^:dynamic *counter* nil)
+
 (defn fake-pool
   [_]
   (let [socket (Socket.)
@@ -12,23 +14,40 @@
         connect (Connection. socket "12323" 123 23)]
     [pool connect]))
 
-(defn fake-replies
-  [replyGen body-fn as-pipeline?]
-  (let [reply (replyGen)]
-    (body-fn)
+(defn- fake-replies [replyGen body-fn as-pipeline?]
+  (let [start @*counter*
+        _ (body-fn)
+        reply (if (fn? replyGen)
+                (replyGen)
+                (take (- @*counter* start) (second (split-at start replyGen))))]
     (if (and as-pipeline? (not (vector? reply)))
-      (reverse (into [] reply))
-      reply)))
+      (vec reply)
+      (if (and (seq? reply) (= 1 (count reply)))
+        (first reply)
+        reply))))
 
 (defmacro mock-funs [mocked-funs & body]
   `(with-redefs ~(list* (reduce (fn [ret next]
                                   (let [[command checker] next] (conj ret command checker)))
-                                []
-                                (map (fn [command checker] [command checker]) (take-nth 2 mocked-funs) (take-nth 2 (next mocked-funs)))))
+                          []
+                          (map (fn [command checker] [command checker]) (take-nth 2 mocked-funs) (take-nth 2 (next mocked-funs)))))
+     ~@body))
+
+(defmacro mock-funs-new [mocked-funs & body]
+  `(with-redefs ~(list* (reduce (fn [ret next]
+                                  (let [[command checker] next] (conj ret command checker)))
+                          []
+                          (map (fn [command checker]
+                                 [command `(fn [& args#]
+                                             (swap! *counter* inc)
+                                             (apply ~checker args#))])
+                            (take-nth 2 mocked-funs) (take-nth 2 (next mocked-funs)))))
      ~@body))
 
 (defmacro mock-carmine-redis-client [reply-gen mocked-commands & body]
-  `(let [reply# ~reply-gen]
-     (with-redefs [conn/pooled-conn fake-pool
-                   protocol/with-replies* (partial fake-replies reply#)]
-       (mock-funs [~@mocked-commands] ~@body))))
+  `(binding [*counter* (atom 0)]
+     (let [reply# ~reply-gen]
+       (with-redefs [conn/pooled-conn fake-pool
+                     protocol/with-replies* (partial fake-replies reply#)]
+         (mock-funs-new [~@mocked-commands] ~@body)
+         (reset! *counter* 0)))))
