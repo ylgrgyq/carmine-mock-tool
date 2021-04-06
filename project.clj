5c3444829cef73260950f7224d3f369013498319
liuchong <mail@liuchong.io>
2021-04-06 11:59:03 +0800

    (chore) bump version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carmine-mock-tool "0.3.0-SNAPSHOT"
+(defproject carmine-mock-tool "0.3.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
